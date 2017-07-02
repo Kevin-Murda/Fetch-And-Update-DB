1a69b82b1bfc6b633752460a2541371ca19d6a75
Kevin Rudissaar <kevin.rudissaar@gmail.com>
2017-07-02 17:09:46 +0100

    Fixed indentation.

--- update-db.sh
+++ update-db.sh
@@ -2,7 +2,7 @@
 
 # If configure file exists, then execute it.
 if [[ -f './update-db.conf' ]]; then
-  source './update-db.conf'
+    source './update-db.conf'
 fi
 
 # Prompt user for credentials if none provided.
@@ -11,15 +11,15 @@ while [[ -z ${REMOTE_HOST} ]]; do echo -n '> Enter host/IP of remote server: '; 
 
 while [[ -z ${REMOTE_DB_USER} ]]; do echo -n '> Enter username of remote database: '; read REMOTE_DB_USER; done
 if [[ ${REMOTE_USER_HAS_PASS} -eq 1 ]]; then
-  while [[ -z ${REMOTE_DB_PASS} ]]; do echo -n '> Enter password of remote database: '; read REMOTE_DB_PASS; done
-  REMOTE_DB_PASS="--password=${REMOTE_DB_PASS}"
+    while [[ -z ${REMOTE_DB_PASS} ]]; do echo -n '> Enter password of remote database: '; read REMOTE_DB_PASS; done
+    REMOTE_DB_PASS="--password=${REMOTE_DB_PASS}"
 fi
 while [[ -z ${REMOTE_DB_NAME} ]]; do echo -n '> Enter name of remote database: '; read REMOTE_DB_NAME; done
 
 while [[ -z ${LOCAL_DB_USER} ]]; do echo -n '> Enter username of local database: '; read LOCAL_DB_USER; done
 if [[ ${LOCAL_USER_HAS_PASS} -eq 1 ]]; then
-  while [[ -z ${LOCAL_DB_PASS} ]]; do echo -n '> Enter password of local database: '; read LOCAL_DB_PASS; done
-  LOCAL_DB_PASS="--password=${LOCAL_DB_PASS}"
+    while [[ -z ${LOCAL_DB_PASS} ]]; do echo -n '> Enter password of local database: '; read LOCAL_DB_PASS; done
+    LOCAL_DB_PASS="--password=${LOCAL_DB_PASS}"
 fi
 while [[ -z ${LOCAL_DB_NAME} ]]; do echo -n '> Enter name of local database: '; read LOCAL_DB_NAME; done
 
@@ -32,15 +32,15 @@ ssh-copy-id ${REMOTE_USER}@${REMOTE_HOST} 2> /dev/null
 
 # Command executed on remote host.
 ssh -T ${REMOTE_USER}@${REMOTE_HOST} <<END
-  mysqldump --user=${REMOTE_DB_USER} ${REMOTE_DB_PASS} ${REMOTE_DB_NAME} > ${SQL_FILE}
+    mysqldump --user=${REMOTE_DB_USER} ${REMOTE_DB_PASS} ${REMOTE_DB_NAME} > ${SQL_FILE}
 END
 
 # Quietly transporting file from remote host to local using scp.
 scp ${REMOTE_USER}@${REMOTE_HOST}:${SQL_FILE} ${SQL_FILE} 1> /dev/null
 
 if [[ ${VIEW_SQLFILE} != 'NO' ]]; then
-  more ${SQL_FILE}
-  echo
+    more ${SQL_FILE}
+    echo
 fi
 
 # Ask if user really wants to continue.
@@ -49,48 +49,48 @@ echo -n '> Do you want to continue? (YES/NO): '; read ANSWER
 
 # If yes then import SQL file into local database.
 if [[ ${ANSWER} = 'YES' ]]; then
-  mysql --user=${LOCAL_DB_USER} ${LOCAL_DB_PASS} ${LOCAL_DB_NAME} < ${SQL_FILE}
-  if [[ "${?}" = '0' ]]; then
-    echo '> Database import executed successfully.'
-  fi
+    mysql --user=${LOCAL_DB_USER} ${LOCAL_DB_PASS} ${LOCAL_DB_NAME} < ${SQL_FILE}
+    if [[ "${?}" = '0' ]]; then
+        echo '> Database import executed successfully.'
+    fi
 else
-  echo '> Script terminated.'
-  exit 0
+    echo '> Script terminated.'
+    exit 0
 fi
 
 # WordPress specific options.
 if [[ ${WORDPRESS} = 'YES' ]]; then
-  if [[ ! -z ${LOCAL_SITE_URL} ]]; then
-    SQL_ONE="UPDATE wp_options SET option_value = '${LOCAL_SITE_URL}' WHERE option_name = 'siteurl'"
-    SQL_TWO="UPDATE wp_options SET option_value = '${LOCAL_SITE_URL}' WHERE option_name = 'home'"
-    mysql --user=${LOCAL_DB_USER} --password=${LOCAL_DB_PASS} ${LOCAL_DB_NAME} -e "${SQL_ONE}; ${SQL_TWO}"
-  fi
+    if [[ ! -z ${LOCAL_SITE_URL} ]]; then
+        SQL_ONE="UPDATE wp_options SET option_value = '${LOCAL_SITE_URL}' WHERE option_name = 'siteurl'"
+        SQL_TWO="UPDATE wp_options SET option_value = '${LOCAL_SITE_URL}' WHERE option_name = 'home'"
+        mysql --user=${LOCAL_DB_USER} --password=${LOCAL_DB_PASS} ${LOCAL_DB_NAME} -e "${SQL_ONE}; ${SQL_TWO}"
+    fi
 fi
 
 # Magento specific options.
 if [[ ${MAGENTO} = 'YES' ]]; then
-  if [[ ! -z ${LOCAL_SITE_URL_HTTP} ]]; then
-    SQL="UPDATE core_config_data SET value = '${LOCAL_SITE_URL_HTTP}' WHERE path = 'web/unsecure/base_url'"
-    mysql --user=${LOCAL_DB_USER} --password=${LOCAL_DB_PASS} ${LOCAL_DB_NAME} -e "${SQL}"
-  fi
-  if [[ ! -z ${LOCAL_SITE_URL_HTTPS} ]]; then
-    SQL="UPDATE core_config_data SET value = '${LOCAL_SITE_URL_HTTPS}' WHERE path = 'web/secure/base_url'"
-    mysql --user=${LOCAL_DB_USER} --password=${LOCAL_DB_PASS} ${LOCAL_DB_NAME} -e "${SQL}"
-  fi
+    if [[ ! -z ${LOCAL_SITE_URL_HTTP} ]]; then
+        SQL="UPDATE core_config_data SET value = '${LOCAL_SITE_URL_HTTP}' WHERE path = 'web/unsecure/base_url'"
+        mysql --user=${LOCAL_DB_USER} --password=${LOCAL_DB_PASS} ${LOCAL_DB_NAME} -e "${SQL}"
+    fi
+    if [[ ! -z ${LOCAL_SITE_URL_HTTPS} ]]; then
+        SQL="UPDATE core_config_data SET value = '${LOCAL_SITE_URL_HTTPS}' WHERE path = 'web/secure/base_url'"
+        mysql --user=${LOCAL_DB_USER} --password=${LOCAL_DB_PASS} ${LOCAL_DB_NAME} -e "${SQL}"
+    fi
 fi
 
 # Remove remote SQL file?
 if [[ ${REMOVE_REMOTE_SQL} = 'YES' ]]; then
-  ssh -T ${REMOTE_USER}@${REMOTE_HOST} "rm ${SQL_FILE}"
-  if [[ "${?}" = '0' ]]; then
-    echo '> Remote SQL file removed successfully.'
-  fi
+    ssh -T ${REMOTE_USER}@${REMOTE_HOST} "rm ${SQL_FILE}"
+    if [[ "${?}" = '0' ]]; then
+        echo '> Remote SQL file removed successfully.'
+    fi
 fi
 
 # Remove local SQL file?
 if [[ ${REMOVE_LOCAL_SQL} = 'YES' ]]; then
-  rm ${SQL_FILE}
-  if [[ "${?}" = '0' ]]; then
-    echo '> Local SQL file removed successfully.'
-  fi
+    rm ${SQL_FILE}
+    if [[ "${?}" = '0' ]]; then
+        echo '> Local SQL file removed successfully.'
+    fi
 fi
